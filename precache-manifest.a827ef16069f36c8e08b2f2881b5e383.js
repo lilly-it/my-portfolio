50e7f64da4af00dc641c3e2e591c88631388b03c
liliaDDD <lilia.dorofeev@gmail.com>
2022-02-09 22:48:01 +0100

    Updates

---
+++ precache-manifest.a827ef16069f36c8e08b2f2881b5e383.js
@@ -0,0 +1,134 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b7e1bdc72276fffc5d385e471a453d1",
+    "url": "/my-portfolio/index.html"
+  },
+  {
+    "revision": "35449dfb062a977279d4",
+    "url": "/my-portfolio/static/js/2.071711c6.chunk.js"
+  },
+  {
+    "revision": "aa4100970f46d7e0ec2af84de3f2740b",
+    "url": "/my-portfolio/static/js/2.071711c6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cd618654b6e6c57314ac",
+    "url": "/my-portfolio/static/js/main.7528f51b.chunk.js"
+  },
+  {
+    "revision": "f241adf7bba56ceeeb82",
+    "url": "/my-portfolio/static/js/runtime-main.8b00eace.js"
+  },
+  {
+    "revision": "37df2be3fb8f8fda6732bbeeb3495ea5",
+    "url": "/my-portfolio/static/media/123.37df2be3.jpg"
+  },
+  {
+    "revision": "a1d2ff7b5a3dce1eadcb0f62eb4ef13a",
+    "url": "/my-portfolio/static/media/8678.a1d2ff7b.jpg"
+  },
+  {
+    "revision": "ff49f2b70024ef5958fc84dd0661dbdb",
+    "url": "/my-portfolio/static/media/adaptability.ff49f2b7.svg"
+  },
+  {
+    "revision": "f5c64a5f820c4ac31423ed0ee5d95c7c",
+    "url": "/my-portfolio/static/media/clock.f5c64a5f.svg"
+  },
+  {
+    "revision": "1314401323e1cf81917e30e93eea08e0",
+    "url": "/my-portfolio/static/media/communication.13144013.svg"
+  },
+  {
+    "revision": "d3c3847fb4fe9d88cbab1d1cb8d0b588",
+    "url": "/my-portfolio/static/media/creativeness.d3c3847f.svg"
+  },
+  {
+    "revision": "f620483184a3efa896127ee80cab531c",
+    "url": "/my-portfolio/static/media/diaphragm.f6204831.svg"
+  },
+  {
+    "revision": "3ec6fe5b4a8acef737fec1f3d0bd0647",
+    "url": "/my-portfolio/static/media/efficiency.3ec6fe5b.svg"
+  },
+  {
+    "revision": "1136cf89fdb048e9230c284d9d0ed441",
+    "url": "/my-portfolio/static/media/es6.1136cf89.svg"
+  },
+  {
+    "revision": "b77811b785e85dc2db23b0b169f2bf53",
+    "url": "/my-portfolio/static/media/eth.b77811b7.png"
+  },
+  {
+    "revision": "e9ec51ee2dde6f478d602060bf769327",
+    "url": "/my-portfolio/static/media/figma.e9ec51ee.svg"
+  },
+  {
+    "revision": "3b50cd7b050fcc6720f79acb376392ee",
+    "url": "/my-portfolio/static/media/git-icon.3b50cd7b.svg"
+  },
+  {
+    "revision": "e149f59c6323b18f98e987cd564e1fc9",
+    "url": "/my-portfolio/static/media/good-times2.e149f59c.jpg"
+  },
+  {
+    "revision": "169a9b3ae8eff8e916dc7711c7546c64",
+    "url": "/my-portfolio/static/media/goodtimes-small.169a9b3a.png"
+  },
+  {
+    "revision": "0f7de9f1ea98c8d8e5f6f06632d0dcc7",
+    "url": "/my-portfolio/static/media/javascript.0f7de9f1.svg"
+  },
+  {
+    "revision": "d1749735b44aafa17334e7710263aac3",
+    "url": "/my-portfolio/static/media/money.d1749735.svg"
+  },
+  {
+    "revision": "1d7ab1799a3ddefc10c5a8878a49873a",
+    "url": "/my-portfolio/static/media/nextjs.1d7ab179.svg"
+  },
+  {
+    "revision": "4d2e61452b5d7e1be0f8d25acef0c1f1",
+    "url": "/my-portfolio/static/media/nodejs.4d2e6145.svg"
+  },
+  {
+    "revision": "f656803a4f73383b091048920b7d20ae",
+    "url": "/my-portfolio/static/media/problem-solving.f656803a.svg"
+  },
+  {
+    "revision": "774cc3e445a109ea73b4c6de795ddb14",
+    "url": "/my-portfolio/static/media/radio.774cc3e4.png"
+  },
+  {
+    "revision": "d443972461457602463064bdb6ef5599",
+    "url": "/my-portfolio/static/media/radio2.d4439724.png"
+  },
+  {
+    "revision": "ee3642c3800003b3007a7ce1c1554a0c",
+    "url": "/my-portfolio/static/media/react.ee3642c3.svg"
+  },
+  {
+    "revision": "04d65ff81beb38edbb86b02a57b81845",
+    "url": "/my-portfolio/static/media/redux.04d65ff8.svg"
+  },
+  {
+    "revision": "6b0dea66eec3054894c052f171aaf1bb",
+    "url": "/my-portfolio/static/media/sass.6b0dea66.svg"
+  },
+  {
+    "revision": "75ae4215d3b848ab2562835f69fab287",
+    "url": "/my-portfolio/static/media/softskills.75ae4215.jpg"
+  },
+  {
+    "revision": "01aa9e4f91047f6d13a124ec94bf19ff",
+    "url": "/my-portfolio/static/media/stress.01aa9e4f.svg"
+  },
+  {
+    "revision": "2413e92520ffaeb5bea3575fbd63224a",
+    "url": "/my-portfolio/static/media/tailwindcss-icon.2413e925.svg"
+  },
+  {
+    "revision": "8c795ad57e55b06d76b8a73b27a13fd1",
+    "url": "/my-portfolio/static/media/teamwork.8c795ad5.svg"
+  }
+]);
